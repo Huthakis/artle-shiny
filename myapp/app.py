9d2c371087bfa880bfca1490bb035f1c7ca505b7
Huthakis <109891233+Huthakis@users.noreply.github.com>
2024-08-28 08:56:49 +1200

    adding code and data

---
+++ myapp/app.py
@@ -0,0 +1,95 @@
+#%% Package Imports
+from funs import get_clue, questioner, remove_singles
+from shiny import App, reactive, render, ui
+from pathlib import Path
+import pandas as pd
+import random as rd
+
+#%% Data Preparation
+app_dir = Path(__file__).parent
+
+# Load data
+useful_cols = ['artist', 'country', 'yearOfBirth', 'yearOfDeath',
+               'name', 'year', 'price', 'material',
+               'height', 'width', 'link', 'source']
+art_df = pd.read_csv(app_dir / 'sdata.txt', sep='\t', usecols=useful_cols)
+art_df = remove_singles(art_df)
+
+# Game setup
+rand_number = rd.randint(0, len(art_df) - 1)
+# Get and display image of random art
+url = art_df.source.loc[rand_number]
+filename = art_df.source.loc[rand_number].split('/')[-1]
+
+
+#%% Frontend Definition
+app_ui = ui.page_fluid(
+    ui.row(
+        ui.column(11,ui.panel_title('Artle Shiny!')),
+        ui.column(1, ui.input_dark_mode())
+    ),
+    ui.h3('How much is this art worth?'),
+    ui.img(src=url, width='320px'),
+    #ui.h6('Enter your guess!'),
+    ui.row(
+        ui.column(6, ui.output_ui('input_hider')),
+        ui.column(6, ui.input_action_button("submit", "Submit"))
+    ),
+    ui.output_text('outcome'),
+    #ui.output_text('round_number'),
+    #ui.output_text('real_price'),
+    ui.output_text('clue')
+)
+
+#%% Backend Definition
+def server(input, output, session):
+    # Initialize variables
+    tolerance = 0.1
+
+    @render.ui
+    def input_hider():
+        rounds = input.submit()+1
+        if rounds <= 6:
+            return ui.TagList(
+                ui.input_numeric('g', 'Enter your guess!', min=1, value=0),
+            )
+
+    # Give round
+    @output
+    @render.text
+    def round_number():
+        rounds = input.submit()+1
+        return rounds
+    
+    # Give clue
+    @output
+    @render.text
+    def clue():
+        rounds = input.submit()+1
+        clue = get_clue(art_df, rand_number, rounds)
+        return clue
+    
+    # Give answer
+    @output
+    @render.text
+    def real_price():
+        return art_df.price.iloc[rand_number]
+
+    # Render result based on guess
+    @output
+    @render.text
+    @reactive.event(input.submit)
+    def outcome():
+        #answer = input.g()
+        rounds = input.submit()+1
+        out = questioner(art_df, rand_number, input.g(), rounds, tolerance)
+        return out
+
+    @reactive.effect
+    @reactive.event(input.submit)
+    def _():
+        ui.insert_ui(ui.p(input.g()), selector='#g', where='afterEnd')
+
+#%% Run App
+app = App(app_ui, server)
+
---
+++ myapp/funs.py
@@ -0,0 +1,61 @@
+# Packages
+import pandas as pd
+
+# Function to remove artists that only appear once
+def remove_singles(data):
+    # Count the occurrences of each artist
+    artist_counts = data['artist'].value_counts()
+    # Filter artists that appear more than once
+    artists_to_keep = artist_counts[artist_counts > 1].index
+    # Keep only rows where the artist is in the list of artists to keep
+    filtered_data = data[data['artist'].isin(artists_to_keep)]
+    # Return data
+    return filtered_data
+
+def questioner(data, rand_number, guess, rounds, tolerance):
+    # Logic for game
+    if rounds <= 6:
+        # Get user guess
+        answer = guess
+        try:
+            answer = float(answer)
+        except ValueError:
+            answer = 0
+        print('Your answer: $' + str(answer))
+        # Logic for checking guess
+        if answer == data.price.iloc[rand_number]:
+            return f'You are right! The exact price was ${data.price.iloc[rand_number]}.'
+        elif data.price.iloc[rand_number] * (1 - tolerance) <= answer <= data.price.iloc[rand_number] * (1 + tolerance):
+            return f'You are within {100 * tolerance}%. The exact price was ${data.price.iloc[rand_number]}.'
+        elif answer > data.price.iloc[rand_number]:
+            return 'Too High'
+        elif answer < data.price.iloc[rand_number]:
+            return 'Too low'
+        else:
+            pass
+        
+    else:
+        return f'''Out of rounds! The is {data.name.iloc[rand_number]}, by {data.artist.iloc[rand_number]}, and it was auctioned for ${data.price.iloc[rand_number]}'''
+    #os.remove(file)  # Delete the downloaded image file
+
+# Function for future seperation of questioner and clues
+def get_clue(data, rand_number, rounds):
+        if rounds == 1:
+            return 'Make your first guess!'
+        if rounds == 2:
+            return f'The art is called: {data.name.iloc[rand_number]}'
+        elif rounds == 3:
+            return f'The artist is from: {data.country.iloc[rand_number]}'
+        elif rounds == 4:
+            if pd.isna(data.yearOfDeath.iloc[rand_number]):
+                return 'The artist is alive.'
+            else:
+                return f'The artist died in: {data.yearOfDeath.iloc[rand_number]}'
+        elif rounds == 5:
+            return f'The artist is: {data.artist.iloc[rand_number]}'
+        elif rounds == 6:
+            artist_prices = data[data['artist'] == data.artist.iloc[rand_number]].price
+            artist_other = artist_prices[artist_prices != data.price.iloc[rand_number]].sample(n=1).values[0]
+            return f'Other art by this artist has sold for: ${artist_other}'
+        else:
+            return ''
